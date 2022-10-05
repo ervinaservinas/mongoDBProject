b0e68f2c9935563d4fad7099644464fbc26b4e45
Ervinas <ervinas.dok@gmail.com>
2022-10-05 21:38:12 +0300

    test

--- app.js
+++ app.js
@@ -241,14 +241,15 @@ const cors = require('cors')
 const express = require("express")
 const app = express()
 const mongoose = require('mongoose')
-
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
 app.listen(4000)
 
 
-mongoose.connect("mongodb+srv://admin:ervinas@cluster0.is4uj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+mongoose.connect("ATLAS_URI")
     .then(res => {
         console.log("connection good to backend")
     }).catch(e => {
